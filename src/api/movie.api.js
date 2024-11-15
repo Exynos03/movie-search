ecd96ed3e1a25415f621b8d2dfb53f2128d11b4f
Exynos03 <sumitghosh345@gmail.com>
2024-11-16 02:34:44 +0530

    added secured http

--- src/api/movie.api.js
+++ src/api/movie.api.js
@@ -3,7 +3,7 @@ import axios from "axios"
 
 export const getMovieDetailsByTitle = async (title) => {
     try {
-        const res = await axios.get(`http://www.omdbapi.com/?t=${title}&apikey=b8c1cbfe`)
+        const res = await axios.get(`https://www.omdbapi.com/?t=${title}&apikey=b8c1cbfe`)
 
         if(res.data.Response === "True") return res.data
         else {
@@ -18,7 +18,7 @@ export const getMovieDetailsByTitle = async (title) => {
 export const getMovieSearchDetailsByChar = async (searchParam, pageNum) => {
 
     try {
-        const res = await axios.get(`http://www.omdbapi.com/?s=${searchParam}&apikey=b8c1cbfe&page=${pageNum+1}`)
+        const res = await axios.get(`https://www.omdbapi.com/?s=${searchParam}&apikey=b8c1cbfe&page=${pageNum+1}`)
         if(res.data.Response === "True") return res.data
         else {
             alert(`${res?.data.Error}. Add some more characters to search`)
